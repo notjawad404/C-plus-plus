03e1cc7c582038526304fe1f257c912fcf978bc8
notjawad404 <jawadali000123@gmail.com>
2023-12-26 00:02:12 +0500

    Added Input-Output.cpp

---
+++ Input-Output.cpp
@@ -0,0 +1,15 @@
+// Input 3 Numbers and print/output their sum
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    double n1, n2, n3, sum;
+    
+    cin>> n1 >> n2 >> n3;
+    sum = n1+n2+n3;
+    cout<< sum;
+    return 0;
+}
