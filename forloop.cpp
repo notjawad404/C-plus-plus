84e6ecdbae161a51d71a87ed259cb1a6ee687a01
notjawad404 <jawadali000123@gmail.com>
2023-12-26 23:27:47 +0500

    Added For loop

---
+++ forloop.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int num, num2;
+    cin>>num>> num2 ;
+    
+    for(num; num<= num2; num++){
+        if(num == 0){
+        cout<<"zero"<<endl;
+    }
+    else if(num == 1){
+        cout<<"one"<<endl;
+    }
+    else if(num == 2){
+        cout<<"two"<<endl;
+    }
+    else if(num == 3){
+        cout<<"three"<<endl;
+    }
+    else if(num == 4){
+        cout<<"four"<<endl;
+    }
+    else if(num == 5){
+        cout<<"five"<<endl;
+    }
+    else if(num == 6){
+        cout<<"six"<<endl;
+    }
+    else if(num == 7){
+        cout<<"seven"<<endl;
+    }
+    else if(num == 8){
+        cout<<"eight"<<endl;
+    }
+    else if(num == 9){
+        cout<<"nine"<<endl;
+    }
+    else if(num%2 == 0){
+        cout<<"even"<<endl;
+    }
+    else{
+        cout<<"odd"<<endl;
+    }
+    }
+    return 0;
+}
