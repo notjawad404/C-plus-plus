d400fc582d17b2e8111f39ba28e90a487c595b5f
notjawad404 <jawadali000123@gmail.com>
2024-01-02 00:54:55 +0500

    Added pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -1,9 +1,23 @@
+// Description: Use pointers to compute the sum and absolute difference of two integers.
+
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(){
-    int a = 10;
-    int *p = &a;
-    cout << "a = " << a << endl;
+void update(int *a, int *b)
+{
+    int sum = *a + *b;
+    int differ = abs(*a - *b);
+    cout << sum << endl;
+    cout << differ << endl;
+}
+
+int main()
+{
+    int a, b;
+    int *pa = &a, *pb = &b;
+    cin >> a >> b;
+    update(pa, pb);
+
     return 0;
 }
